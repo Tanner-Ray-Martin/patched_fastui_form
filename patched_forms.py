99b9156f88127cadde2c8d463a395e6b488fc5ae
Tanner Martin <105985436+Tanner-Ray-Martin@users.noreply.github.com>
2024-08-18 06:38:33 -0500

    Create patched_forms.py
    [from working example here](https://github.com/dimonlime/ERP-system/blob/698ec52820fc76a28754edd33a8dca56f8c12882/main.py#L104)

---
+++ patched_forms.py
@@ -0,0 +1,26 @@
+from fastapi import params as fastapi_params
+import pydantic
+import fastui.forms
+import fastapi
+
+def patched_fastui_form(model: type[fastui.forms.FormModel]) -> fastapi_params.Depends:
+    async def run_fastui_form(request: fastapi.Request):
+        async with request.form() as form_data:
+            model_data = fastui.forms.unflatten(form_data)
+
+            try:
+                yield model.model_validate(model_data)
+            except pydantic.ValidationError as e:
+                raise fastapi.HTTPException(
+                    status_code=422,
+                    detail={
+                        "form": e.errors(
+                            include_input=False,
+                            include_url=False,
+                            include_context=False,
+                        )
+                    },
+                )
+
+    return fastapi.Depends(run_fastui_form)
+
